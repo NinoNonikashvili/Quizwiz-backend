5070a7d92d81554087bef80f6a0fa7f600abf671
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-03-28 19:36:54 +0400

    add sanctum

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
+});
+
+Route::post('/login', function () {
+	return 'just about to login';
 });
