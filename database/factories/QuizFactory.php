74e623cdd64a8b7fcdb5aabce79f07df112c16b4
Nino Nonikashvili <nonikashvilinino8799@gmail.com>
2024-04-24 16:21:29 +0400

    fix: create directory from code for images

--- database/factories/QuizFactory.php
+++ database/factories/QuizFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Quiz>
@@ -16,6 +17,9 @@ class QuizFactory extends Factory
 	 */
 	public function definition(): array
 	{
+		if (!Storage::exists('public/quizImages')) {
+			Storage::makeDirectory('public/quizImages');
+		}
 		return [
 			'title'        => fake()->name(),
 			'instructions' => fake()->sentence(),
